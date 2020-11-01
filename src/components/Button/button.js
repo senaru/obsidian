32f27c22ed727752597adfc83bb5d840ce4d8280
Senaru <s3696653@student.rmit.edu.au>
2020-11-01 20:46:15 +1100

    Update Button + Story for Button Component
    Added more attributes

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -3,20 +3,25 @@ import PropTypes from 'prop-types';
 import "./style.scss";
 
 /**
- * @param   {[type]}  children  [children description]
- * @param   {[type]}  href      [href description]
- * @param   {[type]}  variant   [variant description]
- * @param   {[type]}  type      [type description]
- * @param   {[type]}  onClick   [onClick description]
+ * Button Component
  *
- * @return  {[type]}            [return description]
+ * @param   {[type]}  children    [children description]
+ * @param   {[type]}  href        [href description]
+ * @param   {[type]}  variant     [variant description]
+ * @param   {[type]}  type        [type description]
+ * @param   {[type]}  onClick     [onClick description]
+ * @param   {[type]}  disabled    [disabled description]
+ * @param   {[type]}  formAction  [formAction description]
+ * @param   {[type]}  size        [formAction description]
+ *
+ * @return  {[type]}              [return description]
  */
 /**
  * A simple functional component that accepts filtered props similar to an HTML button
  */
-export function Button({ children, href, variant, type, onClick }) {
+export function Button({ children, href, variant, type, onClick, disabled, formAction, size }) {
 
-    const className = `button ${variant}`
+    const className = `button ${variant} ${size}`
 
     if (href) {
         return (
@@ -31,27 +36,50 @@ export function Button({ children, href, variant, type, onClick }) {
     }
 
     return (
-        <button className={className} onClick={onClick}>
+        <button className={className} onClick={onClick} disabled={disabled} formAction={formAction}>
             {children}
         </button>
     )
 }
-
-Button.displayName = "Button"
 
 Button.propTypes = {
     /**
      * Children node of the component
      */
     children: PropTypes.node,
+    /**
+     * Get an href for an Anchor Button
+     */
     href: PropTypes.string,
+    /**
+     * Get the variant to determine the style of the button
+     */
     variant: PropTypes.string,
-    onClick: PropTypes.func,
+    /**
+     * Select type for Input Buttons
+     */
     type: PropTypes.oneOf(["submit", "reset", "button"]),
+    /**
+     * Determine the size of the Button component
+     */
+    size: PropTypes.string,
+    /**
+     * onClick function of the component
+     */
+    onClick: PropTypes.func,
+    /**
+     * Disable the button component
+     */
+    disabled: PropTypes.bool,
+    /**
+     * Provides a formAction for the component
+     */
+    formAction: PropTypes.string
 }
 
 Button.defaultProps = {
-    onClick: () => console.log("Clicked!")
+    variant: "Default",
+    disabled: false
 }
 
 export default Button
