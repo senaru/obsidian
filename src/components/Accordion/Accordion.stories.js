61b4d5e6cfc1c22a68c79c463ffab51d2c36b3bd
Senaru <s3696653@student.rmit.edu.au>
2020-11-01 21:29:33 +1100

    Add Story for Accordion Component + Add Comments to Accordion

---
+++ src/components/Accordion/Accordion.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Accordion } from './Accordion';
+
+export default {
+    title: 'Example/Accordion',
+    component: Accordion,
+};
+
+const Template = ({ children, ...args }) => <Accordion {...args} >{children}</Accordion>;
+
+export const Default = Template.bind({});
+Default.args = {
+    children: "I'm the Content Inside",
+    header: "I'm an Accordion"
+};
+
+
--- src/components/Accordion/accordion.js
+++ src/components/Accordion/accordion.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import styles from "./Accordion.module.scss"
 
+/**
+ * @param   {[Node]}  children    Content to be displayed inside the accordion
+ * @param   {[String]}  header    Header to be show for the accordion
+ *
+ * @return  {[Component]}            Returns the Accordion Component
+ */
+/**
+ * A Simple Accordion Component that accepts a header and content to be displayed inside
+ * */
 export function Accordion({ children, header }) {
 
     var handleClick = (e) => {
@@ -16,17 +25,24 @@ export function Accordion({ children, header }) {
     }
 
     return (
-        <>
+        <Fragment>
             <button className={styles.accordion} onClick={handleClick}>{header}</button>
             <div className={styles["accordion-panel"]}>
                 {children}
             </div>
-        </>
+        </Fragment>
     )
 }
 
 Accordion.propTypes = {
-    header: String
+    /**
+     * Header to be show for the accordion
+     */
+    header: PropTypes.string,
+    /**
+     * Content to be displayed inside the accordion
+     */
+    children: PropTypes.node
 }
 
 export default Accordion
